ed619c1ae7d21135526b8fe45210f6a0fc5558d5
Adrien Callewaert <76485285+AdrienCallewaert@users.noreply.github.com>
2023-03-29 15:04:56 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
 import * as THREE from './assets/cours/applications/libs/three.js-r132/build/three.module';
 
-console.log("THREE", THREE);
+document.addEventListener('DOMContentLoaded', () => {
+    const scene = new THREE.scene();
+
+    const geometry = THREE.BoxGeometry(1, 1, 1); /* Width - Height - depth */ /* largeur - hauteur - profondeur*/
+    const material = THREE.MeshBasicMaterial({ color: "#OOOOFF" });
+    const cube = new THREE.Mesh(geometry, material);
+});
