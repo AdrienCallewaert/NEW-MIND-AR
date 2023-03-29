94cd48094a3c7caa47362ccac87b09dd0801b1ba
Adrien Callewaert <76485285+AdrienCallewaert@users.noreply.github.com>
2023-03-29 16:49:49 +0200

    up

--- main.js
+++ main.js
@@ -1,24 +1,33 @@
-import * as THREE from "./three.module";
-
+import * as THREE from '../../libs/three.js-r132/build/three.module.js';
 
 document.addEventListener('DOMContentLoaded', () => {
     const scene = new THREE.Scene();
 
-    const geometry = THREE.BoxGeometry(1, 1, 1); /* Width - Height - depth */ /* largeur - hauteur - profondeur*/
-    const material = THREE.MeshBasicMaterial({ color: "#OOOOFF" }); /* Color, texture, shininess, etc*/ /* couleur, texture,brillance, etc*/
+    const geometry = new THREE.BoxGeometry(1, 1, 1); /* Width - Height - depth */ /* largeur - hauteur - profondeur*/
+    const material = new THREE.MeshBasicMaterial({ color: "#0000FF" }); /* Color, texture, shininess, etc*/ /* couleur, texture,brillance, etc*/
     const cube = new THREE.Mesh(geometry, material);
-
-    scene.add(cube);
     cube.position.set(0, 0, -2);
     cube.rotation.set(0, Math.PI / 4, 0);
+    scene.add(cube);
 
     const camera = new THREE.PerspectiveCamera();
     camera.position.set(1, 1, 5);
 
-    const renderer = new THREE.WebGLRenderer();
+    const renderer = new THREE.WebGLRenderer({ alpha: true });
     renderer.setSize(500, 500); /* unit in px */ /* unité en pixel */
     renderer.render(scene, camera);
 
-    document.body.appendChild(renderer.domElement);
+    const video = document.createElement("video");
+    navigator.mediaDevices.getUserMedia({ video: true }).then((stream) => {
+        video.srcObject = stream;
+        video.play();
+    });
 
+    video.style.position = "absolute";
+    video.style.width = renderer.domElement.width;
+    video.style.height = renderer.domElement.height;
+    renderer.domElement.style.position = "absolute";
+
+    document.body.appendChild(video);
+    document.body.appendChild(renderer.domElement);
 });
