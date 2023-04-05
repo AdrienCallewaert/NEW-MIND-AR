1a3b01e31016d13cb52b09f015e5c09156b82758
Adrien Callewaert <76485285+AdrienCallewaert@users.noreply.github.com>
2023-04-05 11:04:15 +0200

    up
    tracking et mappping

--- main.js
+++ main.js
@@ -1,25 +1,24 @@
-import { mockWithVideo, mockWithImage } from './assets/applications/libs/camera-mock.js';
 const THREE = window.MINDAR.IMAGE.THREE;
 
 document.addEventListener('DOMContentLoaded', () => {
     const start = async() => {
-
-        //mockWithVideo('./assets/applications/assets/mock-videos/course-banner1.mp4');
-        mockWithImage('./assets/applications/assets/mock-videos/course-banner1.png');
-
+        // initialize MindAR 
         const mindarThree = new window.MINDAR.IMAGE.MindARThree({
             container: document.body,
             imageTargetSrc: './assets/applications/assets/targets/course-banner.mind',
         });
         const { renderer, scene, camera } = mindarThree;
 
+        // create AR object
         const geometry = new THREE.PlaneGeometry(1, 1);
         const material = new THREE.MeshBasicMaterial({ color: 0x00ffff, transparent: true, opacity: 0.5 });
         const plane = new THREE.Mesh(geometry, material);
 
+        // create anchor
         const anchor = mindarThree.addAnchor(0);
         anchor.group.add(plane);
 
+        // start AR
         await mindarThree.start();
         renderer.setAnimationLoop(() => {
             renderer.render(scene, camera);
